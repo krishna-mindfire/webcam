bc2fb6c087c8fed4174c7ed56a792411cf0de7bb
krishna-mindfire <krishna.k@mindfiresolutions.com>
2022-09-19 11:33:33 +0530

    rear camera functionality add

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -66,7 +66,7 @@ export default function Camera() {
       {
         
         element.classList.remove("player");
-        setFlipCamera('user')
+        setFlipCamera('')
       }
       else { 
         element.classList.add("player");
