627df0aad624784a4f688bb82d5edb7dbb4441c4
krishna-mindfire <krishna.k@mindfiresolutions.com>
2022-09-19 15:11:48 +0530

    rear camera functionality add

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -9,8 +9,28 @@ export default function Camera() {
     const [downloadLink, setDownloadLink] = useState('');
     const [name, setName] = useState('')
     const [cameraMode, setFlipCamera] = useState({facingMode : 'environment'});
+    const [isMobileDevice, setDevice] = useState(false)
 
+    let details = navigator.userAgent;
+    let regexp = /android|iphone|kindle|ipad/i;
+    let isMobile = regexp.test(details);
+    
     useEffect(() => {
+      setDevice(isMobile);
+      let element = document.getElementById("player");
+      console.log(isMobileDevice);
+     if (!isMobileDevice) {
+        
+        setFlipCamera({...cameraMode, ['facingMode'] : 'user' })
+        
+        element.classList.add("player");
+      } 
+      else{
+        setDevice(true)
+        element.classList.add("player");
+      }
+      console.log(cameraMode);
+
         navigator.mediaDevices
         .getUserMedia({ video: { facingMode: cameraMode.facingMode, width: 300 } })
         .then(stream => {
@@ -33,20 +53,22 @@ export default function Camera() {
       let photo = photoRef.current;
       let ctx = photo.getContext("2d");
   
-      const width = 1130;
-      const height = 840;
+      // const width = 1130;
+      // const height = 840;
+      const width = videoRef.current.clientWidth;
+      const height = videoRef.current.clientHeight;
       photo.width = width;
       photo.height = height;
-      if(cameraMode.facingMode != 'environment')
+      console.log(cameraMode.facingMode);
+      if(isMobileDevice)
       {
         ctx.translate(width, 0);
         ctx.scale(-1, 1);
       }
-      else
+      else if(cameraMode.facingMode != 'environment')
       {
-        // ctx.translate(width, 0);
-        // ctx.scale(-1, 1);
-        //ctx.resetTransform();
+        ctx.translate(width, 0);
+        ctx.scale(-1, 1);
       }
       
       return setInterval(() => {
@@ -70,23 +92,20 @@ export default function Camera() {
       strip.insertBefore(link, strip.firstChild);
     };
     const flipCamera = (mode) => {
-      console.log(mode);
+    
       //element.classList.remove("player");
       //setFlipCamera({...cameraMode, ['facingMode'] : 'user' })
       var element = document.getElementById("player");
-      if(cameraMode.facingMode === 'user') 
+      if(mode == 'user') 
       {
-        
         element.classList.add("player");
         setFlipCamera({...cameraMode, ['facingMode'] : 'environment' })
-        
       }
       else { 
-
         element.classList.remove("player");
         setFlipCamera({...cameraMode, ['facingMode'] : 'user' })
       }
-
+      console.log(cameraMode);
       // console.log(cameraMode.facingMode);
     }
     return (
@@ -94,7 +113,7 @@ export default function Camera() {
       
         <div className="webcam-video">
           <div className="col-md-12">
-          <video width="100%" heigth="300px" onCanPlay={() => paintToCanvas()} ref={videoRef} id="player" className="player text-center">
+          <video style={{'width' : '100%', 'height':'auto'}} onCanPlay={() => paintToCanvas()} ref={videoRef} id="player" className="player text-center">
             <source src="path-to-video.mp4#t=0.001" type="video/mp4" />
           </video>
               {/* <video width="100%" heigth="300px"
@@ -113,10 +132,10 @@ export default function Camera() {
             </form>
           </div>
           <div className="col-md-6">
-          <button onClick={() => flipCamera(cameraMode.facingMode)} className="btn btn-info">Flip Camera</button>&nbsp;
+          {/* <button onClick={() => flipCamera(cameraMode.facingMode)} className="btn btn-info" disabled={!isMobileDevice}>Flip Camera</button>&nbsp; */}
           <button onClick={() => takePhoto()} className="btn btn-info">Take a photo</button>&nbsp;
           {
-              downloadLink && name ?  <a href={downloadLink ?? '#'} className="btn btn-success" download={name}>Download</a> : <a className="btn btn-danger" disabled >Download</a>
+              downloadLink && name ?  <a href={downloadLink ?? '#'} className="btn bt n-success" download={name}>Download</a> : <a className="btn btn-danger" disabled >Download</a>
             }
           </div>
           <hr/>
